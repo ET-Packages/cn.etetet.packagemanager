6a6419fad7b492c30904572836493bce61ff6597
Lsy <379338943@qq.com>
2024-07-20 20:20:17 +0800

    基础结构初始化

---
+++ Editor/Base/BasePackageToolModule.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEngine;
+
+namespace ET.PackageManager.Editor
+{
+    [HideReferenceObjectPicker]
+    public abstract class BasePackageToolModule : BaseCreateModule
+    {
+        [HideInInspector]
+        public OdinMenuEditorWindow AutoTool { get; internal set; }
+
+        [HideInInspector]
+        public OdinMenuTree Tree { get; internal set; }
+
+        [HideInInspector]
+        public string ModuleName { get; internal set; }
+
+        [HideInInspector]
+        public object UserData { get; internal set; }
+
+        public virtual void SelectionMenu()
+        {
+        }
+    }
+}
---
+++ Editor/Window/Attribute/ETPackageMenuAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ET.PackageManager.Editor
+{
+    public class ETPackageMenuAttribute : Attribute
+    {
+        public string MenuName;
+
+        public int Order;
+
+        public ETPackageMenuAttribute(string menuName, int order = 0)
+        {
+            MenuName = menuName;
+            Order    = order;
+        }
+    }
+
+    public class ETPackageAutoMenuData
+    {
+        public Type Type;
+
+        public string MenuName;
+
+        public int Order;
+    }
+}
