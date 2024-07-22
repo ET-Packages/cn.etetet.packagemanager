4b6daeb96bed9b8d93a0e3e24a60ded96e9aa3bc
Lsy <379338943@qq.com>
2024-07-22 18:54:04 +0800

    基础数据结构

---
+++ Editor/Window/HubModule/Assets/PackageHubAsset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace ET.PackageManager.Editor
+{
+    public class PackageHubAsset : SerializedScriptableObject
+    {
+        public List<PackageHubData> AllPackages = new();
+    }
+}
---
+++ Editor/Window/HubModule/Assets/PackageHubData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Sirenix.Serialization;
+
+namespace ET.PackageManager.Editor
+{
+    [Serializable]
+    public class PackageHubData
+    {
+    }
+}
